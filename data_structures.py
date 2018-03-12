a7b14a314a5712663245fa0b1b2a0825d3edd5cf
Annie <anniekovac95@gmail.com>
2018-03-12 16:57:59 +0100

    Defined initial conditions for losing energy (dictionaries in the beginning), added definition of function update_energy

--- data_structures.py
+++ data_structures.py
@@ -3,8 +3,18 @@ import rospy
 mussel_working_modes = ["sleep", "normal", "camera", "motors", "charging"]
 apad_working_modes = ["motors", "charging_self", "charging_mussels", "sleep"]
 
-mussel_mode_percentages = dict()  # how much energy mussel loses during certain activity
-apad_mode_percentages = dict()  # how much energy mussel loses during certain activity
+# how much energy mussel loses during certain activity (percentage/half an hour)
+mussel_mode_percentages = {"sleep": -1,
+                           "normal": -3,
+                           "camera": -5,
+                           "motors": -10,
+                           "charging": 1}
+
+# how much energy mussel loses during certain activity
+apad_mode_percentages = {"sleep": -1,
+                         "charging_mussels": -2,  # for each mussel
+                         "charging_self": 1,
+                         "motors": -7}
 
 charge_till = 90
 
@@ -12,6 +22,14 @@ charge_till = 90
 class EnergyBase(object):
     def __init__(self, energy):
         self.energy = energy
+
+    def update_energy(self):
+        """
+        This function should calculate change of energy
+        during certain deltaT and certain working state of
+        aMussels and aPad.
+        """
+        pass
 
     def charge(self, charge_duration, charge_till_percentage = False):
         """
@@ -24,6 +42,8 @@ class EnergyBase(object):
         if charge_till:
             while self.energy < charge_till:
                 self.energy += 0.00001
+            charge_duration = rospy.get_rostime().secs - start
+            return charge_duration 
         else:
             finish_time = start + charge_duration
             while rospy.get_rostime().secs < finish_time:
@@ -36,10 +56,12 @@ class EnergyBase(object):
         by charging mussels, but also by moving)
         :param discharge_duration: int (duration of discharge in seconds)
         """
+        print("aPad energy before discharge: {}".format(self.energy))
         start = rospy.get_rostime().secs
         finish_time = start + discharge_duration
         while rospy.get_rostime().secs < finish_time:
             self.energy -= 0.00001
+        print("aPad energy after discharge: {}".format(self.energy))
 
 
 class aMussel(EnergyBase):
@@ -78,17 +100,16 @@ class aPad(EnergyBase):
         """
         print("Energy of mussel before charging: {}".format(mussel_instance.energy))
         self.mussels_charging.append(mussel_instance)
-        mussel_instance.charge(5, charge_till_percentage=True)
+        charge_dur = mussel_instance.charge(5, charge_till_percentage=True)
+        if charge_dur:
+            self.losing_energy(charge_dur)
         print("Energy of mussel after charging: {}".format(mussel_instance.energy))
 
 
-# pitanje : sto s vremenom i punjenjem, praznjenjem ?
-# kako definirati punjenje, praznjenje u kontekstu vremena?
-# kako predstaviti vrijeme = u rosu je u sekundama, treba li skalirati?
 if __name__ == '__main__':
     my_mussel = aMussel(50)
     my_pad = aPad(10)
-    rospy.init_node('arm_to_pos', anonymous=True)
+    rospy.init_node('system', anonymous=True)
     time = rospy.Time().now()
     my_pad.add_mussel_to_charge(my_mussel)
 
