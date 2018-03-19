03af7321baec11259649287d4928dff868dfb85a
Annie <anniekovac95@gmail.com>
2018-03-19 17:13:55 +0100

    Changed first scenario

--- scenarios.py
+++ scenarios.py
@@ -1,19 +1,51 @@
 import data_structures
 import rospy
+import matplotlib.pyplot as plt
+import numpy
+
+
+def plot_energy(energy, time, title="Energy of certain agent"):
+    """
+    PLotting energy of an agent.
+    :param energy: array
+    :param time: array
+    """
+    # turning seconds into half an hour
+    new_time = []
+    first_second = time[0]
+    for sec in time:
+        new_time.append((sec-first_second) * 0.5)
+    time = new_time
+
+    # plotting
+    plt.plot(numpy.array(time), numpy.array(energy))
+    plt.title(title)
+    plt.xlabel("Time (hours)")
+    plt.ylabel("Energy (percent)")
+    plt.grid()
+    plt.show()
 
 
 def scenario1():
     """
     One simple scenario: one aPad is added
-    and one aMussel. aMussel has the energy of
-    23 and aPad has the energy of 98.
-    aPad adds aMussel to charging. WIth this act,
-    aPad loses energy and aMussel gains energy.
-    :return:
+    and one aMussel. aPad has the energy of 22.
+    aPad is charging, therefore, gaining energy.
     """
-    mussel = data_structures.aMussel(23)
-    apad = data_structures.aPad(98)
-    apad.add_mussel_to_charge(mussel)
+    deltat = 1
+    apad = data_structures.aPad(22)
+    energy = []
+    time = []
+    r = rospy.Rate(1/deltat)  # 1 Hz
+    while not rospy.is_shutdown():
+        energy.append(apad.energy)
+        time.append(rospy.get_rostime().secs)
+        apad.update_energy(deltat, working_mode="charging_self")
+        print(apad.energy)
+        if len(energy) > 100:
+            break
+        r.sleep()
+    plot_energy(energy, time, "Energy of an apad in scenario 1 - just charging")
 
 
 if __name__ == '__main__':
