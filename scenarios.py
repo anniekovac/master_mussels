e95463da097a6fdd91781f181ab2931f066fe8b3
Annie <anniekovac95@gmail.com>
2018-03-19 17:49:50 +0100

    Added another scenario

--- scenarios.py
+++ scenarios.py
@@ -7,7 +7,8 @@ import numpy
 
 def plot_energy(energy, time, title="Energy of certain agent"):
     """
-    PLotting energy of an agent.
+    PLotting energy of an agent. For now, one second in "our time" is
+    represented as 30 minutes of simulated time.
     :param energy: array
     :param time: array
     """
@@ -38,13 +39,14 @@ def scenario1():
     """
     deltat = 1
     apad = data_structures.aPad(22)
+    apad.working_mode.append("charging_self")
     energy = []
     time = []
     r = rospy.Rate(1/deltat)  # 1 Hz
     while not rospy.is_shutdown():
         energy.append(apad.energy)
         time.append(rospy.get_rostime().secs)
-        apad.update_energy(deltat, working_mode="charging_self")
+        apad.update_energy(deltat)
         print(apad.energy)
         if len(energy) > 100:
             break
@@ -52,7 +54,35 @@ def scenario1():
     plot_energy(energy, time, "Energy of an apad in scenario 1 - just charging")
 
 
+def scenario2():
+    """
+    There is one mussel in topology.
+    """
+    deltat = 1
+    r = rospy.Rate(1/deltat)
+
+    amussel = data_structures.aMussel(98)
+    modes = [("camera", 7), ("sleep", 2), ("charging", 6), ("camera", 8), ("normal", 6)]
+
+    energy = []
+    time = []
+    for mode, mode_time in modes:
+        start_mode = rospy.get_rostime().secs
+        while not rospy.is_shutdown():
+            if rospy.get_rostime().secs - start_mode >= mode_time:
+                break
+            amussel.working_mode = [mode]
+            energy.append(amussel.energy)
+            time.append(rospy.get_rostime().secs)
+            amussel.update_energy(deltat)
+            print(amussel.energy)
+            if len(energy) > 100:
+                break
+            r.sleep()
+    plot_energy(energy, time, "Energy of amussel in scenario 2")
+
+
 if __name__ == '__main__':
     rospy.init_node('arm_to_pos', anonymous=True)
     time = rospy.Time().now()
-    scenario1()
+    scenario2()
